7da2beb81d4d29419d112cfab1d9fb7f3f90f84b
hp@SWAYAM <swayam24sahoo@gmail.com>
2025-08-07 17:48:48 +0530

    Added frontend URL

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const expenseRoutes = require('./routes/expenseRoutes');
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ['https://smart-split-frontend.vercel.app'],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
